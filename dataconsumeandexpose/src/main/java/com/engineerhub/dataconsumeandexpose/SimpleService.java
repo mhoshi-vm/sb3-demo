e70649396a2a5c5920fc9cac282ac931c99b8670
machih <machih@vmware.com>
2023-05-23 14:02:31 +0900

    Fix some code for later

--- dataconsumeandexpose/src/main/java/com/engineerhub/dataconsumeandexpose/SimpleService.java
+++ dataconsumeandexpose/src/main/java/com/engineerhub/dataconsumeandexpose/SimpleService.java
@@ -1,10 +1,14 @@
 package com.engineerhub.dataconsumeandexpose;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleService {
 
+    private static final Logger log = LoggerFactory.getLogger(SimpleService.class);
     SimpleRepository simpleRepository;
 
     public SimpleService(SimpleRepository simpleRepository) {
@@ -17,5 +21,13 @@ public class SimpleService {
 
     public void save(SimpleEntity entity){
         simpleRepository.save(entity);
+    }
+
+    @RabbitListener(queues = "demoQueue")
+    public void consume(String data){
+        log.info("new data from queue");
+        SimpleEntity simpleEntity = new SimpleEntity();
+        simpleEntity.setData(data);
+        save(simpleEntity);
     }
 }
--- dataproducer/src/main/java/com/engineerhub/dataproducer/QueueProducer.java
+++ dataproducer/src/main/java/com/engineerhub/dataproducer/QueueProducer.java
@@ -1,5 +1,7 @@
 package com.engineerhub.dataproducer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class QueueProducer {
 
+    private static final Logger log = LoggerFactory.getLogger(QueueProducer.class);
     RabbitTemplate rabbitTemplate;
 
     public QueueProducer(RabbitTemplate rabbitTemplate) {
@@ -20,6 +23,7 @@ public class QueueProducer {
     }
 
     public void handle(String data) {
+        log.info("add to demo queue");
         this.rabbitTemplate.convertAndSend("demoQueue", data);
     }
 
